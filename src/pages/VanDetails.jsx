b691d254a9318fa1c4604a653db012312afdc067
KamiKruse <altsake@protonmail.com>
2025-03-26 12:27:04 +0530

    Finished the van details page with styling with working backlinks and routes

--- src/pages/VanDetails.jsx
+++ src/pages/VanDetails.jsx
@@ -1,22 +1,32 @@
 import { Link, useParams } from "react-router-dom";
+import { ArrowLeft} from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function VanDetails() {
-    const [vanDetail, setVanDetail] = useState({})
-    const params = useParams()
-    useEffect(()=>{
-        const vanDeets = async() =>{
-            try {
-                const response = await fetch(`/api/vans/${params.id}`)
-                const result = await response.json()
-                setVanDetail(result.vans)
-            } catch (error) {
-                console.log("Error fetching error: ", error)
-            }
-        }
-        vanDeets()
-    }, [params])
-    
+  const [vanDetail, setVanDetail] = useState({});
+  const params = useParams();
+  useEffect(() => {
+    const vanDeets = async () => {
+      try {
+        const response = await fetch(`/api/vans/${params.id}`);
+        const result = await response.json();
+        setVanDetail(result.vans);
+      } catch (error) {
+        console.log("Error fetching error: ", error);
+      }
+    };
+    vanDeets();
+  }, [params]);
+  const tagSwitch = () => {
+    if (vanDetail.type === "luxury") {
+      return "van-tag-luxury";
+    } else if (vanDetail.type === "rugged") {
+      return "van-tag-rugged";
+    } else {
+      return "van-tag-simple";
+    }
+  };
+
   return (
     <>
       <header className="nav-bar">
@@ -32,9 +42,28 @@ export default function VanDetails() {
           </Link>
         </nav>
       </header>
+      <Link to="/vans" className="vans-page-backlink">
+        <ArrowLeft size={18} color="grey" />
+        <p className="vansDetails-link-text">Back to all Vans</p>
+      </Link>
       <main className="van-details-main">
-        <Link to="/vans" className="vans-page-backlink">Back to all Vans</Link>
-        <img src={vanDetail.imageUrl} alt={`Image of ${vanDetail.name}`} />
+        <img
+          src={vanDetail.imageUrl}
+          alt={`Image of ${vanDetail.name}`}
+          className="vandetails-image"
+        />
+        <section className="vanDetails-blurb">
+          <div className={tagSwitch()}>{vanDetail.type}</div>
+          <p className="vanDetails-heading">{vanDetail.name}</p>
+          <p className="vanDetails-price">${vanDetail.price}/day</p>
+          <p className="vanDetails-body">
+            The Modest Explorer is a van designed to get you out of the house
+            and into nature. This beauty is equipped with solar panels, a
+            composting toilet, a water tank and kitchenette. The idea is that
+            you can pack up your home and escape for a weekend or even longer!
+          </p>
+          <button className="vanDetails-btn">Rent this Van</button>
+        </section>
       </main>
       <footer>2022 #VANLIFE</footer>
     </>
